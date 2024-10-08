3f4dfcb02127387bbf985a40ff57a2dcb97398d4
Samuel Monteiro <smf.monteiro1701@gmail.com>
2024-10-08 10:57:48 -0300

    Add files via upload

---
+++ minha-aplicacao-web (1)/minha-aplicacao-web/src/main/java/com/seuprojeto/minhaaplicacaoweb/MinhaAplicacaoWebApplication.java
@@ -0,0 +1,13 @@
+package com.seuprojeto.minhaaplicacaoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaAplicacaoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaAplicacaoWebApplication.class, args);
+	}
+
+}
---
+++ minha-aplicacao-web (1)/minha-aplicacao-web/src/test/java/com/seuprojeto/minhaaplicacaoweb/MinhaAplicacaoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seuprojeto.minhaaplicacaoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaAplicacaoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
